1c3673792a141d3eb7d6eed51c0bb9ff980c5ec3
Vitaliy <salinskiyvitaliy@gmail.com>
2024-01-18 23:50:25 +0100

    product detail page implementation commit

--- src/app/pages/product-details-page/product-details-page.component.ts
+++ src/app/pages/product-details-page/product-details-page.component.ts
@@ -5,5 +5,24 @@ import { Component } from '@angular/core';
 	templateUrl: './product-details-page.component.html',
 })
 export class ProductDetailsPageComponent {
+	colors: string[] = ['#FFD700', '#228B22', '#1E90FF', '#483D8B', '#B22222'];
+	sizes: string[] = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+	activeSize: string = 'XS'
+	count: number = 1;
 
+	decreaseCount() {
+		if (this.count > 1) {
+			this.count--;
+		}
+	}
+
+	increaseCount() {
+		if (this.count < 99) {
+			this.count++;
+		}
+	}
+
+	onSizeChange(value: string) {
+		this.activeSize = value;
+	}
 }
